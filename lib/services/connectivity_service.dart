e0488a240f1acbc2eac0775bd1b0c915490ac9e8
Mahmood Hassan <32261587+mahmoodh12332@users.noreply.github.com>
2021-11-26 15:46:03 +0500

    style

--- lib/services/connectivity_service.dart
+++ lib/services/connectivity_service.dart
@@ -1,28 +1,28 @@
 import 'dart:async';
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-class ConnectivityService{
+class ConnectivityService {
   ConnectivityResult _connectionStatus = ConnectivityResult.none;
   final Connectivity _connectivity = Connectivity();
   late StreamSubscription<ConnectivityResult> _connectivitySubscription;
   ProviderReference? _ref;
-  ConnectivityService({ProviderReference? ref}){
+  ConnectivityService({ProviderReference? ref}) {
     _ref = ref;
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
   }
 
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-      _connectionStatus = result;
+    _connectionStatus = result;
   }
 
   checkInternetConnection() async {
     try {
       _connectionStatus = await _connectivity.checkConnectivity();
-    }  catch (e) {
+    } catch (e) {
       print(e.toString());
       return;
     }
--- lib/views/chat/constants.dart
+++ lib/views/chat/constants.dart
@@ -6,4 +6,4 @@ const kContentColorLightTheme = Color(0xFF1D1D35);
 const kContentColorDarkTheme = Color(0xFFF5FCF9);
 const kWarninngColor = Color(0xFFF3BB1C);
 const kErrorColor = Color(0xFFF03738);
-const kDefaultPadding = 20.0;
+const kDefaultPadding = 10.0;

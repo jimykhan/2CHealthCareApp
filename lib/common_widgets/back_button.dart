149c27b06ae48edba2be8a5cdafe69a4ab2b88b5
jimmykhan <jkbjd007@gmail.com>
2021-12-27 11:59:09 +0500

    change backButton

---
+++ lib/common_widgets/back_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ()=>Navigator.pop(context),
+      child: Container(
+        padding: EdgeInsets.only(right: 10,top: 10,bottom: 10),
+        decoration: BoxDecoration(
+        ),
+        child: Icon(Icons.arrow_back_ios,
+        color: Colors.black,),
+      ),
+    );
+  }
+}
